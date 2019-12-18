56d21b4960308aea6b9545ef92feb3c690a9735d
Mohamed Ahmed <error17191@gmail.com>
2019-12-18 18:39:26 +0200

    Game is now playable

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'fetchOnImages.dart';
+import "dart:async";
+
+List gameCards = [];
+var cardsOnFront = [];
 
 void main() {
   getCardsForGame();
+  for (var i = 0; i < 12; i++) {
+    gameCards.add(GameCard(index: i));
+  }
   runApp(MyApp());
 }
 
@@ -31,31 +40,30 @@ class _MyAppState extends State<MyApp> {
               Expanded(
                 child: Row(
                   children: <Widget>[
-                    GameCard(index: 0),
-                    GameCard(index: 1),
-                    GameCard(index: 2),
-                    GameCard(index: 3),
+                    gameCards[0],
+                    gameCards[1],
+                    gameCards[2],
+                    gameCards[3],
                   ],
                 ),
               ),
               Expanded(
                 child: Row(
                   children: <Widget>[
-                    GameCard(index: 4),
-                    GameCard(index: 5),
-                    GameCard(index: 6),
-                    GameCard(index: 7),
+                    gameCards[4],
+                    gameCards[5],
+                    gameCards[6],
+                    gameCards[7],
                   ],
                 ),
               ),
               Expanded(
                 child: Row(
                   children: <Widget>[
-                    GameCard(index: 8),
-                    GameCard(index: 9),
-                    GameCard(index: 10),
-                    GameCard(index: 11),
-
+                    gameCards[8],
+                    gameCards[9],
+                    gameCards[10],
+                    gameCards[11],
                   ],
                 ),
               ),
@@ -67,51 +75,104 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
 class GameCard extends StatefulWidget {
   int index;
+  _GameCardState state;
 
   GameCard({Key key, this.index}) : super(key: key);
 
+  void clearImage(){
+    state.clearMe();
+  }
+
+  void flipOnBack(){
+    state.flipOnBack();
+  }
+
   @override
-  _GameCardState createState() => _GameCardState(index: index);
+  _GameCardState createState() {
+    state = _GameCardState(index: index);
+    return state;
+  }
+
 }
 
 class _GameCardState extends State<GameCard> {
   int index;
   String imagePath;
   bool isOnBack = true;
+  bool done = false;
 
   String imgCover = 'assets/images/cover.png';
 
   _GameCardState({this.index});
-
 
   void initState() {
     super.initState();
     imagePath = imgCover;
   }
 
+  void clearMe() {
+    setState(() {
+      done = true;
+    });
+  }
+  void flipOnBack(){
+    setState(() {
+      imagePath = imgCover;
+      isOnBack = true;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         child: FlatButton(
           onPressed: () {
+            if (! isOnBack || cardsOnFront.length == 2) {
+              return;
+            }
+
             setState(() {
-              if(isOnBack){
-                print("this is");
-                print(index);
-                imagePath = images[getItemOfList(index)];
-              }else{
-                imagePath = imgCover;
+
+              cardsOnFront.add({
+                'name': getItemOfList(index),
+                'index': index
+              });
+
+              imagePath = images[getItemOfList(index)];
+              isOnBack = false;
+
+              if (cardsOnFront.length == 2 && cardsOnFront[0]['name'] == cardsOnFront[1]['name']) {
+                new Future.delayed(Duration(seconds : 1), (){
+                  for (var i = 0; i < cardsOnFront.length; i++) {
+                    gameCards[cardsOnFront[i]['index']].clearImage();
+                  }
+                  cardsOnFront = [];
+                });
+
+                return;
               }
+
+              if(cardsOnFront.length == 2){
+                new Future.delayed(Duration(seconds : 2), (){
+                  for(var i=0; i< cardsOnFront.length; i++){
+                    gameCards[cardsOnFront[i]['index']].flipOnBack();
+                  }
+                  cardsOnFront = [];
+                });
+              }
+
+
             });
-            this.isOnBack = ! this.isOnBack;
           },
-          child: Image.asset(imagePath),
+          child: showCard(imagePath, done),
         ),
       ),
     );
   }
+}
+
+Widget showCard(path, done) {
+  return done ? Text("") : Image.asset(path);
 }
